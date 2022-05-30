3c76bc07114e7f280e3c9060e0a68e287f84a368
Camilo Rodriguez <camiepisode@outlook.com>
2022-05-30 08:30:13 -0500

    Fix: Preventing code fails

--- src/WhatsAppCloudApi.js
+++ src/WhatsAppCloudApi.js
@@ -7,17 +7,29 @@ class WhatsAppCloudApi {
   }
 
   sendMessage (data) {
+    if (!data || !data.message || !data.message.body) {
+      throw new Error('Required WhatsApp data to process this message')
+    }
+
     return this.#send(data)
   }
 
   #send (data) {
+    if (!this._appToken) {
+      throw new Error('Required WhatsApp Token to process this message')
+    }
+
+    if (!data.phone_number_id) {
+      throw new Error('Required WhatsApp phone_number_id to process this message')
+    }
+
     return axios({
       method: 'POST',
       url: `${constants.META_API_ENDPOINT}${constants.META_API_VERSION}/${data.phone_number_id}/${constants.MESSAGES.ENTRY_POINT}${this._appToken}`,
       data: {
         messaging_product: constants.MESSAGING_PRODUCT,
         to: data.from,
-        text: { body: data.message.body },
+        text: { body: data.message.body || '' },
       },
       headers: { 'Content-Type': 'application/json' },
     })
